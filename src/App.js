63a8e3e28463388025a93be0633997fa439f3619
Artem Khan-Magomedov <dagermeister@mail.ru>
2022-11-01 18:22:16 +0300

    step to props

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Card from "./components/Card"
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Card 
+        img="katie-zaferes.png"
+        rating="5.0"
+        reviewCount={6}
+        country="Whatever you want"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+      />
     </div>
   );
 }
